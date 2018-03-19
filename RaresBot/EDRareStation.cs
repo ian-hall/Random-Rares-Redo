b29152fa5aaaae5b011e712f8be92bbbf787ae8e
ian <ian-hall@users.noreply.github.com>
2018-03-19 13:43:48 -0700

    initial

---
+++ RaresBot/EDRareStation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Json;
+
+namespace RaresBot
+{
+    class EDRareStation
+    {
+        public string Item { get; set; }
+        public string System { get; set; }
+        public string Port { get; set; }
+        public int Dst { get; set; }
+        public int Alloc { get; set; }
+        public int Cost { get; set; }
+        public bool Permit { get; set; }
+        public bool Illeg { get; set; }
+        public int ID { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public EDRareStation(JsonValue json, string item)
+        {
+            //Need to null check on alloc and cost
+            string checkAlloc = json["alloc"];
+            string checkCost = json["cost"];
+            this.Item = item;
+            this.System = json["system"];
+            this.Port = json["port"];
+            this.Dst = json["dst"];
+            this.Alloc = (checkAlloc != "") ? int.Parse(checkAlloc) : 0;
+            this.Cost = (checkCost != "") ? int.Parse(checkCost) : 0;
+            this.Permit = json["permit"];
+            this.Illeg = json["illeg"];
+            this.ID = json["port_id"];
+            this.X = json["x"];
+            this.Y = json["y"];
+            this.Z = json["z"];
+        }
+
+        override
+        public string ToString()
+        {
+            return String.Format("{0} sold in {1} @ {2} ({3}cr)", this.Item, this.System, this.Port, this.Cost);
+        }
+    }
+}
--- RaresBot/Program.cs
+++ RaresBot/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Json;
 
 namespace RaresBot
 {
@@ -6,7 +9,17 @@ namespace RaresBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var allStuff = JsonConvert.DeserializeObject<EDRareStation>(File.ReadAllText("../../../rares.json"));
+            using (var jsonStream = File.OpenRead("../../../rares.json"))
+            {
+                var raresArray = (JsonObject)JsonObject.Load(jsonStream);
+                foreach( var item in raresArray.Keys)
+                {
+                    var itemInfo = raresArray[item];
+                    Console.WriteLine(new EDRareStation(itemInfo, item));
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
